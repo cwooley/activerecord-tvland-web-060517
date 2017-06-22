d63fd6a8902b1c4bdeec02c1f1e4a0f0337b1034
github username <github email address>
2017-06-22 11:44:53 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(hash)
+    network = Network.create(hash)
+    network.shows << self
+  end
 end
